d2cd715f3cad796108d2d681d3e0f05dde3a33cb
Nitin <ni3patil2601@gmail.com>
2026-08-27 17:15:49 +0530

    FastAPI

---
+++ pyndatic_why.py
@@ -0,0 +1,11 @@
+
+
+def insert_patient_data(name : str, age:int):
+    print(name)
+    print(age)
+    print('Inserted into Database')
+
+insert_patient_data('Nitin','twenty-seven')
+
+insert_patient_data()
+
