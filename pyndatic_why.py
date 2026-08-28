bb44b0e736dd4c0b1705cc60545a7e99ec904a0c
Nitin <ni3patil2601@gmail.com>
2026-08-28 14:08:37 +0530

    FastAPI

--- pyndatic_why.py
+++ pyndatic_why.py
@@ -1,11 +1,26 @@
+from pydantic import BaseModel
+from typing import List
 
+class Patient(BaseModel):
+    name: str
+    age: int
+    weight: float
 
-def insert_patient_data(name : str, age:int):
-    print(name)
-    print(age)
+patient_info = {
+    'name': 'Nitin',
+    'age': '27',
+    'weight':45.5,
+    married:bool,
+    allergies:List[str],
+    
+}
+
+patient1 = Patient(**patient_info)
+
+def insert_patient_data(patient1):
+    print(patient1.name)
+    print(patient1.age)
+    print(patient1.weight)
     print('Inserted into Database')
 
-insert_patient_data('Nitin','twenty-seven')
-
-insert_patient_data()
-
+insert_patient_data(patient1)
